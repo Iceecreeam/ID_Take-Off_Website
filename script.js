efd5e4760af2bf74235b1feb5e0c2334acacbf94
Li Min Kua <s10206234@connect.np.edu.sg>
2021-01-31 23:32:05 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+currInt()
+function currInt() {
+      fetch("https://api.nasa.gov/planetary/apod?api_key=0kDrOBtIYM2fhZoVrtf80AaSIzg4Tb7PPSeJ1bfu")
+         .then(response => response.json())
+         .then(data => data)
+         .then(function(data){
+
+         /*modify json for use*/
+         console.log("hh")
+         console.log(data)
+         
+         })
+}
