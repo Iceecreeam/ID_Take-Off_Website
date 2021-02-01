6f97e3fb9d1ef6b2c3dd7b30069e098161678e29
Li Min Kua <s10206234@connect.np.edu.sg>
2021-02-02 03:53:19 +0800

    Added html, some css and jQuery for photo of the day section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-currInt()
-function currInt() {
+picOfDay()
+function picOfDay() {
       fetch("https://api.nasa.gov/planetary/apod?api_key=0kDrOBtIYM2fhZoVrtf80AaSIzg4Tb7PPSeJ1bfu")
          .then(response => response.json())
          .then(data => data)
          .then(function(data){
 
          /*modify json for use*/
-         console.log("hh")
          console.log(data)
-         
+         $("#photoDay>img").attr("src",data["url"])
          })
 }
