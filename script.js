8092f4e529de4fd91f2c40ea07e9fb9fd3f03774
Li Min Kua <s10206234@connect.np.edu.sg>
2021-02-02 09:37:48 +0800

    Added html, some css and javscript for near earth objects section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*load values into pic of day section*/
 picOfDay()
 function picOfDay() {
       fetch("https://api.nasa.gov/planetary/apod?api_key=0kDrOBtIYM2fhZoVrtf80AaSIzg4Tb7PPSeJ1bfu")
@@ -9,4 +11,24 @@ function picOfDay() {
          console.log(data)
          $("#photoDay>img").attr("src",data["url"])
          })
+}
+
+
+/*load values into near earth objects section*/
+NEO()
+function NEO() {
+      var startDate = "2021-02-01" /*todays date*/
+      var endDate = "2021-02-02" /*tomorrow's date*/
+
+      fetch("https://api.nasa.gov/neo/rest/v1/feed?start_date=" + startDate + "&end_date=" + endDate + "&api_key=0kDrOBtIYM2fhZoVrtf80AaSIzg4Tb7PPSeJ1bfu")
+      .then(response => response.json())
+      .then(data => data["near_earth_objects"])
+      .then(function(data){
+
+      /*modify json for use*/
+      
+      
+
+      console.log(data)
+      })
 }
