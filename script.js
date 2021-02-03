ead33d3deca272d03407b23442dc5f9d97c242a9
Li Min Kua <s10206234@connect.np.edu.sg>
2021-02-03 16:08:04 +0800

    added button to news tile

--- script.js
+++ script.js
@@ -23,12 +23,6 @@ function picOfDay() {
             $("#photoDay>p.desc").append('It seems like this media type is unsupported.')
          }
          
-         let newDate = data["date"]
-         const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(newDate);
-         const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(newDate);
-         const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(newDate);
-         var d = `${da}-${mo}-${ye}`
-         $("#date").text(d)
          $(".imgTit>b").text(data["title"])
          $("#explain").text(data["explanation"])
          $("#copyright").text(data["copyright"])
@@ -60,9 +54,6 @@ function NEO() {
 
       /*check if JSON key with today's date has more than 4 children (youll get it when you see the console).
       if it has less than 4, change numTdy to the number of children*/
-      if (data["2021-02-01"].length > 4){
-            console.log("test");
-      } 
 
       /*check if JSON key with tomorrow's date has more than 4 children (youll get it when you see the console).
       if it has less than 4, change numTmr to the number of children*/
@@ -90,7 +81,7 @@ function NEO() {
             var dia = '0.2' //Get reading in kilometers (average between min and max). 1 d.p. if less than 10. 0 d.p if more than 10
             var vel = '45' //relative_velocity. get km/s reading. 0 d.p.
             var dis = '68' //miss_distance. get km reading. divide by 1 million and make it 0 d.p.
-            addString += '<div class="mb-2 px-3 d-flex justify-content-around text-left neoInfo"><div class="col-4 p-0 neoInfoLeft"><h4 class="neoDateTime">' + dayTime + ' UT</h4> <a href="'+link+'" target="_blank" class="neoName"><b>'+name+'</b></a> </div> <div class="col-7 d-flex p-0 pt-0 neoInfoRight"> <div class="col-4 p-0"> <p class="my-2">Diameter</p> <p><b>'+dia+'</b> km<p> </div> <div class="col-4 p-0"> <p class="my-2">Velocity</p> <p><b>'+ vel +'</b> km/s<p> </div> <div class="col-4 p-0"> <p class="my-2">Closest Dist</p> <p><b>68</b> MM km<p> </div> </div> </div>'
+            addString += '<div class="mb-2 px-3 d-flex justify-content-around text-left neoInfo"><div class="col-4 p-0 neoInfoLeft"><h4 class="neoDateTime">' + dayTime + ' UT</h4> <a href="'+link+'" target="_blank" class="neoName"><b>'+name+'</b></a> </div> <div class="col-7 d-flex p-0 pt-0 neoInfoRight"> <div class="col-4 p-0"> <p class="my-2">Diameter</p> <p><b>'+dia+'</b> km<p> </div> <div class="col-4 p-0"> <p class="my-2">Velocity</p> <p><b>'+ vel +'</b> km/s<p> </div> <div class="col-4 p-0"> <p class="my-2">Closest Dist</p> <p><b>+'+dis+'</b> MM km<p> </div> </div> </div>'
       }
 
       /*tomorrow loop*/
@@ -110,7 +101,23 @@ function NEO() {
 
 
 
-/*change innerhtml of drowpdown button to selected country*/
+/*change innerhtml of drowpdown button to selection*/
 $(document).on("click", ".dropMed" , function() {
       ($(this).parent().siblings("button")).text($(this).text())
    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
