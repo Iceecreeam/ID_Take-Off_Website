337a01fdc92a187cc1cb4c2961e1564507cee72d
Li Min Kua <s10206234@connect.np.edu.sg>
2021-02-16 19:10:21 +0800

    Removed ios detection

--- script.js
+++ script.js
@@ -472,16 +472,3 @@ $("#colour").on("input", function(){
       changecolour()
 });
 
-/*detect ios https://stackoverflow.com/a/9039885*/
-function iOS() {
-      return [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-      ].includes(navigator.platform)
-      // iPad on iOS 13 detection
-      || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-    }
